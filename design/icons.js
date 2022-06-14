bc2a5a09b574592d15e2ccdaed744352f0ba0d4b
dxtmisha <dxtmisha@gmail.com>
2022-06-14 13:12:19 +0700

    Update icons.js

--- design/icons.js
+++ design/icons.js
@@ -1,8 +1,13 @@
 import { Icon } from 'dcode/classes/Icon'
+import forEach from 'dcode/functions/forEach'
 
-export default function () {
-  Icon.add('back-office', require('../assets/icons/back-office.svg'))
-  Icon.add('edit', require('../assets/icons/edit.svg'))
-  Icon.add('help', require('../assets/icons/help.svg'))
-  Icon.add('verified', require('../assets/icons/verified.svg'))
+export default function (icons = undefined) {
+  if (icons) {
+    forEach(icons, (icon, index) => Icon.add(index, icon))
+  } else {
+    Icon.add('back-office', require('../assets/icons/back-office.svg'))
+    Icon.add('edit', require('../assets/icons/edit.svg'))
+    Icon.add('help', require('../assets/icons/help.svg'))
+    Icon.add('verified', require('../assets/icons/verified.svg'))
+  }
 }
